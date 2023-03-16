e995a6e7c96808de825712fda93e3452378af012
Mewah Aprilia Susyanti <111202012801@mhs.dinus.ac.id>
2023-03-16 16:32:15 +0700

    JavaScript

--- Assignment 2/index.js
+++ Assignment 2/index.js
@@ -13,13 +13,12 @@ function tampilkan_form() {
 const Form = document.getElementById("form-data");
 const Nama = document.getElementById("nama");
 const Role = document.getElementById("role");
-const Availability = document.getElementById("avaibility");
 const Usia = document.getElementById("usia");
 const Lokasi = document.getElementById("lokasi");
 const Pengalaman = document.getElementById("pengalaman");
 const Email = document.getElementById("email");
 
-const buttonSubmit = document.getElementById("button1");
+const buttonSubmit = document.getElementById("my-button");
 const Hasil = document.getElementById("hasil");
 
 let isLowerBracketShow = true;
@@ -27,25 +26,25 @@ let isLowerBracketShow = true;
 Form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const Inama = Form.elements.inama.value;
-  const Irole = Form.elements.irole.value;
-  const Iavaibility = Form.elements.iavailability.value;
-  const Iusia = Form.elements.iusia.value;
-  const Ilokasi = Form.elements.ilokasi.value;
-  const Ipengalaman = Form.elements.ipengalaman.value;
-  const Iemail = Form.elements.iemail.value;
+  const inputnama = Form.elements.Nama.value;
+  const inputrole = Form.elements.Role.value;
+  const inputusia = Form.elements.Usia.value;
+  const inputlokasi = Form.elements.Lokasi.value;
+  const inputpengalaman = Form.elements.Pengalaman.value;
+  const inputemail = Form.elements.Email.value;
 
-  inama.innerHTML = Inama;
-  irole.innerHTML = Irole;
-  iavailability.innerHTML = Iavaibility;
-  iusia.innerHTML = Iusia;
-  ilokasi.innerHTML = Ilokasi;
-  ipengalaman.innerHTML = Ipengalaman;
-  iemail.innerHTML = Iemail;
+  Nama.innerHTML = inputnama;
+  Role.innerHTML = inputrole;
+  Usia.innerHTML = inputusia;
+  Lokasi.innerHTML = inputlokasi;
+  Pengalaman.innerHTML = inputpengalaman;
+  Email.innerHTML = inputemail;
 
   tampil_hasil();
+  Form.style.display = "none";
 });
 
+// untuk menampilkan input data yang sudah di edit
 function tampil_hasil() {
   if (isLowerBracketShow) {
     Hasil.isLowerBracketShow = false;
@@ -60,6 +59,30 @@ buttonSubmit.addEventListener("click", (event) => {
   event.preventDefault();
   tampil_hasil();
 });
-// function tampilkan_form() {
-//   document.getElementById("hasil").innerHTML;
-// }
+
+// Menampilkan jendela Allert sukses mengedit
+btn = document.getElementById("submit");
+btn.addEventListener("click", function () {
+  let timerInterval;
+  Swal.fire({
+    title: "Terima kasih",
+    html: "I will close in <b></b> milliseconds.",
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 100);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+    }
+  });
+});
